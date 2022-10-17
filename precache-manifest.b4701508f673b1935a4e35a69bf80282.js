4b3451e5a1e095b64355a7e80641de5a04aa2199
Timothy Paul Ignacio <timothypaulignacio@gmail.com>
2022-10-17 19:04:05 +0800

    Updates

---
+++ precache-manifest.b4701508f673b1935a4e35a69bf80282.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "d775a6737b67b3af3aa8",
+    "url": "/odin-jollibee-clone/static/css/main.0b057ae5.chunk.css"
+  },
+  {
+    "revision": "d775a6737b67b3af3aa8",
+    "url": "/odin-jollibee-clone/static/js/main.88e05714.chunk.js"
+  },
+  {
+    "revision": "1a54e9b4b63509413fb9",
+    "url": "/odin-jollibee-clone/static/js/runtime~main.26375d8c.js"
+  },
+  {
+    "revision": "5c85c02b88a4f4c83233",
+    "url": "/odin-jollibee-clone/static/js/2.9d10601d.chunk.js"
+  },
+  {
+    "revision": "97aff226a8716615d8b820b5af75b597",
+    "url": "/odin-jollibee-clone/static/media/download.97aff226.png"
+  },
+  {
+    "revision": "b7167349eb60a0a1a1d2db854516eab3",
+    "url": "/odin-jollibee-clone/static/media/order_pickup.b7167349.png"
+  },
+  {
+    "revision": "88c6db6aff3fd62e405356b69dd60aa1",
+    "url": "/odin-jollibee-clone/static/media/call.88c6db6a.png"
+  },
+  {
+    "revision": "04f677c009810bf907f0f405b1fdd922",
+    "url": "/odin-jollibee-clone/static/media/promos-funko.04f677c0.jpg"
+  },
+  {
+    "revision": "d7c4a37a87f337d71087aa24ebe1e400",
+    "url": "/odin-jollibee-clone/static/media/what-wedo.d7c4a37a.jpeg"
+  },
+  {
+    "revision": "fb3e80a7fe54e61072e57b2e3303d312",
+    "url": "/odin-jollibee-clone/static/media/what-we-stand.fb3e80a7.jpeg"
+  },
+  {
+    "revision": "aba79432c1082edf7a611c01d080ab31",
+    "url": "/odin-jollibee-clone/static/media/promos-last.aba79432.jpg"
+  },
+  {
+    "revision": "22ebcad64ea5ecd1ce381b5b8149044f",
+    "url": "/odin-jollibee-clone/static/media/hero_banner.22ebcad6.jpeg"
+  },
+  {
+    "revision": "2e5a20f4bb6ae699ed81389970510df6",
+    "url": "/odin-jollibee-clone/static/media/promo-mid.2e5a20f4.png"
+  },
+  {
+    "revision": "6c3bcf1ea48e7a2798cdd5248eccfda2",
+    "url": "/odin-jollibee-clone/static/media/delivery-hero-img.6c3bcf1e.jpeg"
+  },
+  {
+    "revision": "fbc0f640df92809265b53017c91334b3",
+    "url": "/odin-jollibee-clone/static/media/delivery-card-phone.fbc0f640.jpeg"
+  },
+  {
+    "revision": "ba522799cda2c7fee9c58e3a31fa89ca",
+    "url": "/odin-jollibee-clone/static/media/delivery-card-telephone.ba522799.jpeg"
+  },
+  {
+    "revision": "af6d820cd3d3f7b8bae5a91293811165",
+    "url": "/odin-jollibee-clone/static/media/delivery-card-multidelivery.af6d820c.jpeg"
+  },
+  {
+    "revision": "69db9bbdcb77dba873c59dcb213e63fb",
+    "url": "/odin-jollibee-clone/static/media/VAGRoundedStdBlack.69db9bbd.otf"
+  },
+  {
+    "revision": "6a9c01ef14383ff2a4a0498ba1058a34",
+    "url": "/odin-jollibee-clone/static/media/VAGRoundedStdThin.6a9c01ef.otf"
+  },
+  {
+    "revision": "21b0e9c93e5b240b0c3b22793c0f3169",
+    "url": "/odin-jollibee-clone/static/media/VAGRoundedStdLight.21b0e9c9.otf"
+  },
+  {
+    "revision": "ae8812432fb80cb865eaaec5ff473837",
+    "url": "/odin-jollibee-clone/static/media/featured-chicken.ae881243.png"
+  },
+  {
+    "revision": "2a7aa5a1d0bb3cc435d1f062f1e54062",
+    "url": "/odin-jollibee-clone/static/media/featured-spag.2a7aa5a1.png"
+  },
+  {
+    "revision": "6e4ffb4b2bd9b48823a09c7301b7bded",
+    "url": "/odin-jollibee-clone/static/media/featured-burger.6e4ffb4b.png"
+  },
+  {
+    "revision": "47c3838efd408e993954bd73dc128f44",
+    "url": "/odin-jollibee-clone/index.html"
+  }
+];
